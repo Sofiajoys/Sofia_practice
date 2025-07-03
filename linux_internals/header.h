e9c7bcaef7d2b2479cc5ce786391ceac705b32eb
Sofiajoys <sofiajoys2401@gmail.com>
2025-07-03 15:32:37 +0530

    stat

---
+++ linux_internals/header.h
@@ -0,0 +1,11 @@
+#ifndef CAL_H
+#define CAL_H
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a,int b);
+float div(int a,int b);
+int even(int a);
+int odd(int a);
+int prime(int a);
+int fibonacci(int n);
+#endif
