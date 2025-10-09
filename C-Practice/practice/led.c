1d1c4bffb8f7e0c3bf9b10e5a19ebb14ba7a3846
Sofiajoys <sofiajoys2401@gmail.com>
2025-10-09 18:14:17 +0530

    files

--- C-Practice/practice/led.c
+++ C-Practice/practice/led.c
@@ -1,57 +1,23 @@
-clude <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <fcntl.h>
 
-#define LED_BRIGHTNESS "/sys/class/leds/user_led/brightness"
-#define LED_TRIGGER    "/sys/class/leds/user_led/trigger"
-
-void set_trigger_none() {
-    int fd = open(LED_TRIGGER, O_WRONLY);
-    if (fd < 0) {
-        perror("Error opening trigger file");
-        exit(EXIT_FAILURE);
-    }
-
-    if (write(fd, "none", 4) < 0) {
-        perror("Error writing to trigger file");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    close(fd);
-}
-
-void set_brightness(int value) {
-    int fd = open(LED_BRIGHTNESS, O_WRONLY);
-    if (fd < 0) {
-        perror("Error opening brightness file");
-        exit(EXIT_FAILURE);
-    }
-
-    char buffer[4];
-    int len = snprintf(buffer, sizeof(buffer), "%d", value);
-    if (write(fd, buffer, len) < 0) {
-        perror("Error writing brightness");
-    }
-
-    close(fd);
-}
+#define GPIO "/sys/class/gpio/gpio4/value"
 
 int main() {
-    set_trigger_none();  // Disable automatic LED triggers
-
-    printf("Blinking LED using while loop. Press Ctrl+C to stop.\n");
-
-    while (1) {
-        set_brightness(255);  // LED ON
-        sleep(1);             // 1-second delay
-
-        set_brightness(0);    // LED OFF
-        sleep(1);             // 1-second delay
+    int fd = open(GPIO, O_WRONLY);
+    if(fd < 0) {
+        perror("GPIO open");
+        return -1;
     }
 
+    while(1) {
+        write(fd, "1", 1);  // LED ON
+        usleep(500000);     // 500ms delay
+        write(fd, "0", 1);  // LED OFF
+        usleep(500000);     // 500ms delay
+    }
+
+    close(fd);
     return 0;
 }
-
