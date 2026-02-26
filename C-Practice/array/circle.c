74a2e1d3b90efcb46bf266dc1ec4f1c19ea306bb
Sofiajoys <sofiajoys2401@gmail.com>
2026-02-26 17:10:06 +0530

    pattern

--- C-Practice/array/circle.c
+++ C-Practice/array/circle.c
@@ -2,17 +2,17 @@
 #include<math.h>
 int main()
 {
-        printf("enter the radius\n");
-        float r;
-        scanf("%f",&r);
-        float d;
-        for(int i=-2*r;i<=2*r;i++)
-        {       for(int j=-2*r;j<=2*r;j++)
-		{               d=sqrt(i*i + j*j);
-			if(d>r)
-                printf("  ");
-            else
-                printf("* ");
+	printf("enter the radius\n");
+	float r;
+	scanf("%f",&r);
+	float d;
+	for(int i=-r;i<=r;i++)
+	{	for(int j=-r;j<=r;j++)
+		{		d=sqrt(i*i + j*j);
+			if(d<r)
+				printf("* ");
+			else
+				printf("  ");
 }
 printf("\n");
 }
---
+++ C-Practice/patterns/pascal.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter value: ");
+    scanf("%d",&n);
+    int val;
+    for(int i=0;i<=n;i++)
+    {
+        val=1;
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d ",val);
+            val=val*(i-j)/(j+1);
+        }
+        printf("\n");
+    }
+    }    
