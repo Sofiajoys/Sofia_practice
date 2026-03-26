ac2f25790b6148d5dccf78bec35cf4885c42d67b
Sofiajoys <sofiajoys2401@gmail.com>
2026-03-26 12:23:29 +0530

    linkedlist

---
+++ C-Practice/practice2/linkedlist/deletemid.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *newnode(int data)
+{
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
+int main()
+{
+    int n,val;
+    struct node *temp=NULL,*head=NULL;
+    printf("enter no of nodes: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("enter val for node %d: ",i+1);
+        scanf("%d",&val);
+        struct node *new=newnode(val);
+        if(head==NULL)
+        {
+            head=new;
+            temp=head;
+        }
+        else
+        {
+            temp->next=new;
+            temp=temp->next;
+        }
+    }
+    //delete mid
+    struct node *prev=NULL;
+    struct node *del=head;
+    int pos;
+    printf("enter pos to delete: ");
+    scanf("%d",&pos);
+    for(int i=0;i<pos && del!=NULL;i++)
+    {
+        prev=del;
+        del=del->next;
+    }
+    if(del==NULL)
+    {
+        printf("invalid position\n");
+    }
+    else
+    {
+        prev->next=del->next;
+        free(del);
+    }
+    //print
+    printf("Linked list: ");
+    temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d -> ",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+    return 0;
+
+}
---
+++ C-Practice/practice2/linkedlist/insertmid.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *newnode(int data)
+{
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
+int main()
+{
+    int n,val;
+    struct node *temp=NULL,*head=NULL;
+    printf("enter no of nodes: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("enter val for node %d: ",i+1);
+        scanf("%d",&val);
+        struct node *new=newnode(val);
+        if(head==NULL)
+        {
+            head=new;
+            temp=head;
+        }
+        else
+        {
+            temp->next=new;
+            temp=temp->next;
+        }
+    }
+    //insert mid
+    int m,pos;
+    printf("enter value to insert: ");
+    scanf("%d",&m);
+    printf("enter pos: ");
+    scanf("%d",&pos);
+    struct node *mid=newnode(m);
+    temp=head;
+    for(int i=0;i<pos && temp!=NULL;i++)
+    {
+        temp=temp->next;
+    }
+    if(temp!=NULL)
+    {
+        mid->next=temp->next;
+        temp->next=mid;
+    }
+    //print
+    printf("Linked list: ");
+    temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d -> ",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+    return 0;
+}
